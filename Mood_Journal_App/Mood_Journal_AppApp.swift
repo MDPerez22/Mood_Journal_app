634b11f38e874d63d61000bec397d4ee87fa62cc
Marcos Daniel Perez Bernal <marcos@Marcoss-MacBook-Pro.local>
2025-09-07 15:25:44 -0400

    Initial commit

---
+++ Mood_Journal_App/Mood_Journal_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_Journal_AppApp.swift
+//  Mood_Journal_App
+//
+//  Created by Marcos Daniel Perez Bernal on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Mood_Journal_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
